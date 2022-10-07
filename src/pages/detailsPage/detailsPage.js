d61e484b27e478c958f7ad754f151beae680f507
Salman Ahmed <salmanahmedx@hotmail.com>
2022-10-07 23:49:08 +0600

    accordion structure complete

--- src/pages/detailsPage/detailsPage.js
+++ src/pages/detailsPage/detailsPage.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import "./detailsPage.css"
-
+import React, { useEffect, useState } from 'react';
+import "./detailsPage.css";
 
 const DetailsPage = () => {
 
+    const skillDetails = [{
+        "intro": "lorem2 jdaskjf hjska fskja hfakj hfskajhf kjsahf slkajhf skjlahf sjkahf kjah jkah ljkhakj",
+        "details": "123lroe kjfksdh kfha dskjfhs kjfah kjah fkjah fkjahf kjafh kjlah fjklah jkahkj hakj hjkla hjkah kjah jksah kjah kjsah kjah kh lkjah fkjahjkfl ahkj hfajkh sjklfah fsjklafhf jksahjk hajk hkjsah kjash kjahskj hfsakjls hfkja hkjlahkjl"
+    },
+    {
+        "intro": "lorem2 jdaskjf hjska fskja hfakj hfskajhf kjsahf slkajhf skjlahf sjkahf kjah jkah ljkhakj",
+        "details": "123lroe kjfksdh kfha dskjfhs kjfah kjah fkjah fkjahf kjafh kjlah fjklah jkahkj hakj hjkla hjkah kjah jksah kjah kjsah kjah kh lkjah fkjahjkfl ahkj hfajkh sjklfah fsjklafhf jksahjk hajk hkjsah kjash kjahskj hfsakjls hfkja hkjlahkjl"
+    },
+    {
+        "intro": "98f732e iofyiue yf982 yiu2hf io2uy89 fhhy20yf 092",
+        "details": "lroe kjfksdh kfha dskjfhs kjfah kjah fkjah fkjahf kjafh kjlah lorem2 jdaskjf hjska fskja hfakj hfskajhf kjsahf slkajhf skjlahf sjkahf kjah jkah ljkhakj hkjsah kjash kjahskj hfsakjls hfkja hkjlahkjl"
+    },
+    {
+        "intro": "lorem2 jdaskjf hjska fskja hfakj hfskajhf kjsahf slkajhf skjlahf sjkahf kjah jkah ljkhakj",
+        "details": "lroe kjfksdh kfha dskjfhs kjfah kjah fkjah fkjahf kjafh kjlah fjklah jkahkj hakj hjkla hjkah kjah jksah kjah kjsah kjah kh lkjah fkjahjkfl ahkj hfajkh sjklfah fsjklafhf jksahjk hajk hkjsah kjash kjahskj hfsakjls hfkja hkjlahkjl"
+    },
+    {
+        "intro": "98f732e iofyiue yf982 yiu2hf io2uy89 fhhy20yf 092",
+        "details": "lroe kjfksdh kfha dskjfhs kjfah kjah fkjah fkjahf kjafh kjlah lorem2 jdaskjf hjska fskja hfakj hfskajhf kjsahf slkajhf skjlahf sjkahf kjah jkah ljkhakj hkjsah kjash kjahskj hfsakjls hfkja hkjlahkjl"
+    }
+    ]
+
     const expandServiceList = (e) => {
-        console.log(e.target.classList.contains("heading-1-text"))
-        console.log(e.target.parentElement.nextSibling.classList)
-        if (e.target.classList.contains("heading-1-text") && e.target.parentElement.nextSibling.classList.contains("subServices")) {
-            e.target.parentElement.nextSibling.classList.toggle("hide")
+        if ((e.target.classList.contains("heading-1-text")) && (e.target.parentElement.nextSibling.classList.contains("subServices"))) {
+            e.target.parentElement.nextSibling.classList.toggle("hide");
         }
     }
 
     const expandServiceDetails = (e) => {
-        //use switch statement
+        console.log()
+
         if (e.target.classList.contains("subService-1")) {
-            document.querySelector(".service-detials-intro").textContent = "Pardon The fdsafsdafasfdas"
+            document.querySelector(".service-detials-intro").textContent = skillDetails[0].intro;
+            document.querySelector(".service-details").textContent = skillDetails[0].details;
         }
         if (e.target.classList.contains("subService-2")) {
-            document.querySelector(".service-detials-intro").textContent = "Not pardon the hahahaafkhjsdtjhkafukl"
+            document.querySelector(".service-detials-intro").textContent = skillDetails[1].intro;
+            document.querySelector(".service-details").textContent = skillDetails[1].details;
+        }
+        if (e.target.classList.contains("subService-3")) {
+            document.querySelector(".service-detials-intro").textContent = skillDetails[2].intro;
+            document.querySelector(".service-details").textContent = skillDetails[2].details;
+        }
+        if (e.target.classList.contains("subService-4")) {
+            document.querySelector(".service-detials-intro").textContent = skillDetails[3].intro;
+            document.querySelector(".service-details").textContent = skillDetails[3].details;
+        }
+        if (e.target.classList.contains("subService-5")) {
+            document.querySelector(".service-detials-intro").textContent = skillDetails[4].intro;
+            document.querySelector(".service-details").textContent = skillDetails[4].details;
         }
     }
     return (
@@ -33,13 +67,15 @@ const DetailsPage = () => {
                         <div className='collapse-indicator'></div>
                         <h3 className='heading-1-text text-lg font-semibold'>Design</h3>
                     </div>
-                    <div className="subServices-1 subServices">
+                    <div className="subServices-1 subServices ml-4">
                         <div className="subService-1" onClick={expandServiceDetails}>Prototyping</div>
                         <div className="subService-2" onClick={expandServiceDetails}>Animation</div>
                         <div className="subService-3" onClick={expandServiceDetails}>Content Creation</div>
+                        <div className="subService-4" onClick={expandServiceDetails}>Content Creation</div>
+                        <div className="subService-5" onClick={expandServiceDetails}>Content Creation</div>
                     </div>
                 </div>
-                {/* ============================================================ */}
+                {/* =========================================================== */}
 
 
             </div >
@@ -47,7 +83,7 @@ const DetailsPage = () => {
             <div div className="about basis-3/5" onClick={expandServiceList} >
                 <h1 className='animated-about-heading'>Service Details</h1>
                 <h3 className='service-detials-intro'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quas ad praesentium est cupiditate fugiat repellendus natus. Officiis, velit quo?</h3>
-                <p className='service-details-paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur nihil molestias sapiente accusamus unde quia corporis, vitae ipsa magnam placeat suscipit commodi sint ipsam aliquid incidunt aperiam, dignissimos similique iste repellat eaque velit. Corporis.</p>
+                <p className='service-details'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur nihil molestias sapiente accusamus unde quia corporis, vitae ipsa magnam placeat suscipit commodi sint ipsam aliquid incidunt aperiam, dignissimos similique iste repellat eaque velit. Corporis.</p>
             </div >
 
         </div >
